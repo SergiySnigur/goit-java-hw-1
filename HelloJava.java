ce47d9ed3f1e6e2c2a892b591cd1c7fc58580358
unknown <serzhsnigur@gmail.com>
2021-11-26 22:31:31 +0200

    second commit

---
+++ HelloJava.java
@@ -0,0 +1,9 @@
+public class HelloJava {
+        public static void main(String[] args) {
+
+        System.out.print("Hello");
+        System.out.print("");
+        System.out.print("Java");
+
+       }
+}
